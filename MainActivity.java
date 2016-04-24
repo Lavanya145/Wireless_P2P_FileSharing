d521ce32afc2e633f743ac31e4c8f1335cc72969
Lavanya145 <lavanyaaradhya@outlook.com>
2016-04-24 14:19:36 -0500

    Added files via upload

---
+++ MainActivity.java
@@ -0,0 +1,217 @@
+package com.example.lavanyaaradhya.tracker_app;
+
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView infoIp, infoPort;
+    ListView File_list;
+
+    ArrayList<HashMap<String, String>> list_of_hmaps = new ArrayList<HashMap<String, String>>();
+    HashMap<String, String> map = new HashMap<String, String>();
+    ArrayList<String> files_in_peers;
+    ArrayList<String> files_list_ontracker = new ArrayList<String>();
+
+    static final int SocketServerPORT = 8081;
+    ServerSocket serverSocket;
+
+    ServerSocketThread serverSocketThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        infoIp = (TextView) findViewById(R.id.infoip);
+        infoPort = (TextView) findViewById(R.id.infoport);
+        File_list = (ListView) findViewById(R.id.File_list);
+
+        infoIp.setText(getIpAddress());
+
+        serverSocketThread = new ServerSocketThread();
+        serverSocketThread.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "SiteLocalAddress: "
+                                + inetAddress.getHostAddress() + "\n";
+                    }
+
+                }
+
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+
+        return ip;
+    }
+
+    public class ServerSocketThread extends Thread {
+
+        @Override
+        public void run() {
+            Socket socket = null;
+
+            try {
+                serverSocket = new ServerSocket(SocketServerPORT);
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        infoPort.setText("I'm waiting here: "
+                                + serverSocket.getLocalPort());
+                    }});
+
+                while (true) {
+                    socket = serverSocket.accept();
+                    FileTxThread fileTxThread = new FileTxThread(socket);
+                    fileTxThread.start();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    public class FileTxThread extends Thread {
+        Socket socket;
+
+        FileTxThread(Socket socket){
+            this.socket= socket;
+        }
+
+        @Override
+        public void run() {
+            /*File file = new File(
+                    Environment.getExternalStorageDirectory(),
+                    "test.txt");
+
+            byte[] bytes = new byte[(int) file.length()];
+            BufferedInputStream bis;*/
+            try {
+                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                Object object = ois.readObject();
+                files_in_peers = new ArrayList<String>();
+                files_in_peers = (ArrayList<String>) object;
+                for (int x = 0; x < files_in_peers.size(); x++) {
+                    map.put(getIpAddress(), files_in_peers.get(x));
+                }
+                list_of_hmaps.add(map);
+
+               /* bis = new BufferedInputStream(new FileInputStream(file));
+                bis.read(bytes, 0, bytes.length);
+                OutputStream os = socket.getOutputStream();
+                os.write(bytes, 0, bytes.length);
+                os.flush();*/
+                socket.close();
+
+                final String sentMsg = "File sent to: " + socket.getInetAddress();
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        ListAdapter adapters = new SimpleAdapter(getApplicationContext(), list_of_hmaps, R.layout.activity_main,
+                                new String[]{getIpAddress()},
+                                new int[]{R.id.File_list});
+
+                        File_list.setAdapter(adapters);
+                        Toast.makeText(MainActivity.this,
+                                sentMsg,
+                                Toast.LENGTH_LONG).show();
+                    }});
+
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
